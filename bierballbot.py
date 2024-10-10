7397c56942604d147e6f9c7dabfdeba79dc6d48a
kadsendino <maxii.prietzel@gmail.com>
2024-10-10 21:26:40 +0200

    Bierballbot 1 Version

--- bierballbot.py
+++ bierballbot.py
@@ -1,14 +1,21 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
-import os
+import os, random
 from dotenv import load_dotenv
 
 # Load the environment variables from the .env file
 load_dotenv()
 
+fail_texts=["Du scheiß Idioot! Voll daneben geworfen!"]
+success_texts=["Starker Wurf! Flasche getroffen!"]
+
 # Define the /start command handler function
-async def start(update: Update, context) -> None:
-    await update.message.reply_text("Hello World")
+async def wurf(update: Update, context) -> None:
+    if random.randint(1,100) >= 10:
+        await update.message.reply_text(random.choice(fail_texts))
+    else:
+        await update.message.reply_text(random.choice(success_texts))
+
 
 # Main function to set up the bot
 if __name__ == '__main__':
@@ -19,7 +26,7 @@ if __name__ == '__main__':
     app = ApplicationBuilder().token(bot_token).build()
     
     # Add a command handler for the /start command
-    app.add_handler(CommandHandler('start', start))
+    app.add_handler(CommandHandler('wurf', wurf))
     
     # Start the bot
     app.run_polling()
